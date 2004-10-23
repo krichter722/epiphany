5deb403ccbf43344102f92267e18e59c0d6f6014
Xan Lopez <xan@src.gnome.org>
2004-10-23 11:25:02 +0000

    'er we go.
    'er we go.

---
+++ embed/webcore/webcore-embed.h
@@ -0,0 +1,61 @@
+/*
+ *  Copyright (C) 2000-2004 Marco Pesenti Gritti
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  $Id$
+ */
+
+#ifndef WEBCORE_EMBED_H
+#define WEBCORE_EMBED_H
+
+#include "ephy-embed.h"
+
+#include <glib-object.h>
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+#define WEBCORE_TYPE_EMBED		(webcore_embed_get_type ())
+#define WEBCORE_EMBED(o)		(G_TYPE_CHECK_INSTANCE_CAST ((o), WEBCORE_TYPE_EMBED, WebcoreEmbed))
+#define WEBCORE_EMBED_CLASS(k)		(G_TYPE_CHECK_CLASS_CAST((k), WEBCORE_TYPE_EMBED, WebcoreEmbedClass))
+#define WEBCORE_IS_EMBED(o)		(G_TYPE_CHECK_INSTANCE_TYPE ((o), WEBCORE_TYPE_EMBED))
+#define WEBCORE_IS_EMBED_CLASS(k)	(G_TYPE_CHECK_CLASS_TYPE ((k), WEBCORE_TYPE_EMBED))
+#define WEBCORE_EMBED_GET_CLASS(o)	(G_TYPE_INSTANCE_GET_CLASS ((o), WEBCORE_TYPE_EMBED, WebcoreEmbedClass))
+
+typedef struct WebcoreEmbedClass	WebcoreEmbedClass;
+typedef struct WebcoreEmbed		WebcoreEmbed;
+typedef struct WebcoreEmbedPrivate	WebcoreEmbedPrivate;
+
+struct WebcoreEmbed
+{
+        GtkBin parent;
+
+	/*< private >*/
+        WebcoreEmbedPrivate *priv;
+};
+
+struct WebcoreEmbedClass
+{
+        GtkBinClass parent_class;
+};
+
+GType	       webcore_embed_get_type         (void);
+
+gpointer      _webcore_embed_get_ephy_browser (WebcoreEmbed *embed);
+
+G_END_DECLS
+
+#endif
