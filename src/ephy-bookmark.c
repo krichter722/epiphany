8cad7fde2ea315dfe37ee6a21ab80803d1fe6f35
Gabriel Ivascu <ivascu.gabriel59@gmail.com>
2016-08-29 20:20:37 +0300

    ephy-bookmark: Align function parameters

--- src/ephy-bookmark.c
+++ src/ephy-bookmark.c
@@ -197,9 +197,9 @@ ephy_bookmark_init (EphyBookmark *self)
 
 static JsonNode *
 ephy_bookmark_json_serializable_serialize_property (JsonSerializable *serializable,
-                                                    const gchar *name,
-                                                    const GValue *value,
-                                                    GParamSpec *pspec)
+                                                    const char       *name,
+                                                    const GValue     *value,
+                                                    GParamSpec       *pspec)
 {
   JsonNode *node = NULL;
 
@@ -229,10 +229,10 @@ ephy_bookmark_json_serializable_serialize_property (JsonSerializable *serializab
 
 static gboolean
 ephy_bookmark_json_serializable_deserialize_property (JsonSerializable *serializable,
-                                                      const gchar *name,
-                                                      GValue *value,
-                                                      GParamSpec *pspec,
-                                                      JsonNode *node)
+                                                      const char       *name,
+                                                      GValue           *value,
+                                                      GParamSpec       *pspec,
+                                                      JsonNode         *node)
 {
   if (g_strcmp0 (name, "tags") == 0) {
     GSequence *tags;
